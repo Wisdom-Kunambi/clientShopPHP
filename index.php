950933066b16e797755870f2fb36736ac23a13c2
Wisdom-Kunambi <kunambiwaziri1@gmail.com>
2024-06-07 20:50:54 +0300

    add first commit

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Shop</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body>
+     <div class="container my-5">
+        <h2>List of Clients</h2>
+        <a class="btn btn-primary" href="/myshop/create.php" role="button">New Client</a>
+        <br>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Address</th>
+                    <th>Crafted At</th>
+                    <th>Action</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $servername = "localhost";
+                $username = "root";
+                $password = "";
+                $database = "myshop";
+
+                // Create connection
+                $connection = new mysqli($servername, $username, $password, $database);
+
+                // Check connection
+                if ($connection->connect_error) {
+                    die("Connection failed: " . $connection->connect_error);
+                }
+
+                // read all row from database table
+                $sql = "SELECT * FROM clients";
+                $result = $connection->query($sql);
+
+                if (!$result) {
+                    die("Invalid query: " . $connection->error);
+                }
+
+                // read data of each row
+                while ($row = $result->fetch_assoc()) {
+                    echo "
+                    <tr>
+                        <td>" . $row["id"] . "</td>
+                        <td>" . $row["name"] . "</td>
+                        <td>" . $row["email"] . "</td>
+                        <td>" . $row["phone"] . "</td>
+                        <td>" . $row["address"] . "</td>
+                        <td>" . $row["created_at"] . "</td>
+                        <td>
+                            <a class='btn btn-primary btn-sm' href='/myshop/edit.php?id=" . $row["id"] . "' role='button'>Edit</a>
+                            <a class='btn btn-danger btn-sm' href='/myshop/delete.php?id=" . $row["id"] . "' role='button'>Delete</a>
+                        </td>
+                    </tr>
+                    ";
+                }
+                
+                ?>
+                
+            </tbody>
+            
+        </table>
+
+     </div>
+</body>
+
+</html>
